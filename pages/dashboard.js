2f773a5d0513acd7fb91b2240bed09c8fa181a50
casperon56 <casperon56@gmail.com>
2026-04-21 15:44:58 +0300

    Create dashboard.js

---
+++ pages/dashboard.js
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="text-white h-screen flex items-center justify-center text-4xl">
+      Welcome to Fure Dashboard
+    </div>
+  );
+}
