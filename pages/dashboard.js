643e9067bb47836a6b38359269cedfd4f8b8c35b
casperon56 <casperon56@gmail.com>
2026-04-21 15:53:07 +0300

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,17 @@
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
-    <div className="h-screen bg-dark text-white">
+    <div className="h-screen bg-dark text-white flex flex-col">
       <Navbar />
 
-      <div className="flex items-center justify-center h-full text-4xl">
-        Welcome to Fure Dashboard
+      <div className="flex h-full">
+        <Sidebar />
+
+        <div className="flex-1 flex items-center justify-center text-4xl">
+          Welcome to Fure Dashboard
+        </div>
       </div>
     </div>
   );
