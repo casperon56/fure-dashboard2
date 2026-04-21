fe05ff5ff7a7b21ae50ad018f0351e5fe1bd07d2
casperon56 <casperon56@gmail.com>
2026-04-21 15:22:49 +0300

    Update tailwind.config.js
    # Fure Dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#FFD700",
+        dark: "#000000",
+        dark2: "#111111"
+      }
+    }
+  },
+  plugins: [],
+};
