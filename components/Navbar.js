ae031a0b8b98d0fb1a4c51543ab9c6b7a5ac147a
casperon56 <casperon56@gmail.com>
2026-04-21 15:45:53 +0300

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="w-full h-16 bg-dark flex items-center px-6 text-gold text-2xl font-bold">
+      Fure Dashboard
+    </div>
+  );
+}
